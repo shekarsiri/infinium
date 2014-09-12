68fece10a5088127b78684d1a0dbe8e39bd2a2a2
Jerrad Genson <Jerrad Genson@localhost>
2014-09-11 23:09:59 -0500

    Add main controller module skeleton.

---
+++ infinium.py
@@ -0,0 +1,24 @@
+def main():
+    # Parse command line arguments.
+    
+    # Launch user interface.
+    
+    # Connect to database.
+    
+    # Extract testing set from data.
+    
+    if user_interface.load_model:
+        # Load valuation model from disk.
+        pass   
+    
+    else: 
+        # Extract training set from data.
+    
+        # Construct the valuation model from training data.
+    
+        # Save model parameters to disk.
+        pass
+    
+    # Use testing data to evaluate quality of model.
+    
+    # Save testing results to disk.

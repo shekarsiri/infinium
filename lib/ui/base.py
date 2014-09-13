1cca497cfd54d4e31d08c93285b729e92c1de126
Jerrad Genson <Jerrad Genson@localhost>
2014-09-13 00:23:06 -0500

    Add `lib.ui.base` module docstring and header constants.

---
+++ lib/ui/base.py
@@ -0,0 +1,25 @@
+"""
+Contains abstract base class from which all user interfaces should inherit, as
+well as other generic functions and data.
+
+Copyright 2014 Jerrad M. Genson
+
+This file is part of Infinium.
+
+Infinium is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Infinium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Infinium.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+__author__ = 'Jerrad Genson'
+__contact__ = 'jerradgenson@neomailbox.ch'

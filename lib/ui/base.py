41e3c415900cecf39f5232330eabb02c7cad2899
Jerrad Genson <Jerrad Genson@localhost>
2014-09-13 03:38:08 -0500

    Implement `lib.ui.base.UserInterface` - the abstract base class for user interfaces.

--- lib/ui/base.py
+++ lib/ui/base.py
@@ -27,6 +27,7 @@ __contact__ = 'jerradgenson@neomailbox.ch'
 
 # Python standard library imports.
 from enum import Enum
+from abc import ABCMeta, abstractproperty, abstractmethod
 
 
 class EndGoal(Enum):
@@ -38,3 +39,60 @@ class EndGoal(Enum):
     add_database_entry = 2
     analyze_stock = 3
     exit = 4
+
+
+class UserInterface(metaclass=ABCMeta):
+    """
+    Abstract base class that all user interfaces should inherit from.
+    """
+
+    @abstractproperty
+    def end_goal(self):
+        """
+        The end goal of this iteration of the main event loop.
+        Must be a value defined by ``EndGoal``.
+        """
+
+        pass
+
+    @abstractproperty
+    def database_type(self):
+        """
+        The type of database to connect with.
+        Must be a value defined by DatabaseType.
+        """
+
+        pass
+
+    @abstractproperty
+    def database_location(self):
+        """
+        The location of the database to connect with.
+        Depending on the database type, this must be either a ``Path`` object
+        or a ``DatabaseCredentials`` object.
+        """
+
+        pass
+
+    @abstractproperty
+    def model_path(self):
+        """
+        Path to the valuation model file. Must be a ``Path`` object.
+        """
+
+        pass
+
+    @abstractmethod
+    def show_test_results(self, test_results):
+        """
+        Show results from testing the valuation model to the user.
+
+        Args:
+          test_results: ???
+
+        Returns:
+          None
+
+        """
+
+        pass
